651c5a0deea195209e38bcd0c53043b38a6ada58
pspriggs <pjoshuaspriggs@gmail.com>
2019-04-23 06:43:30 -0600

    hw9

---
+++ instagram/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    # username, first_name, last_name, date_joined and email already in AbstractUser
+    description = models.CharField(max_length=500, unique=False, blank=True, null=False, default="")
+    img_profile = models.CharField(max_length=100, unique=False, blank=True, null=False, default="")
+
